ca61965f4fe90000938a0ed77fdd43b128efc31f
brev <phan.brian07@gmail.com>
2025-08-03 15:49:20 -0400

    init

--- src/main/java/com/example/dashboardapi/config/SecurityConfig.java
+++ src/main/java/com/example/dashboardapi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.dashboardapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -44,6 +45,7 @@ public class SecurityConfig {
             )
             .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class)
             .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/login/**", "/error").permitAll()
                 .anyRequest().authenticated()
             )
@@ -52,20 +54,5 @@ public class SecurityConfig {
             .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        String oktaDomain = "https://dev-77801819.okta.com";
-
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", oktaDomain));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
