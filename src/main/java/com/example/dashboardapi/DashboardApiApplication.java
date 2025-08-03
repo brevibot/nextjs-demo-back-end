f8e582f69b0ad150e596eaa861565dde111e6e3e
brev <phan.brian07@gmail.com>
2025-08-03 07:22:46 -0400

    init

--- src/main/java/com/example/dashboardapi/DashboardApiApplication.java
+++ src/main/java/com/example/dashboardapi/DashboardApiApplication.java
@@ -2,10 +2,8 @@ package com.example.dashboardapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class DashboardApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(DashboardApiApplication.class, args);
---
+++ src/main/java/com/example/dashboardapi/config/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package com.example.dashboardapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@Profile("!local")
+public class SchedulingConfig {
+}
